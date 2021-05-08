82cd533cf7e0fc8cc79331449816dad2ab2d59e2
JaredAmaro <79740511+JaredAmaro@users.noreply.github.com>
2021-05-08 11:16:03 -0500

    Tarea9-Analisi del registro de windows
    Lo que hice fue hacer el análisis con el programa y con el editor del registro, así sacar lo que se me pedía, los encripte aquí están los programas que utilice para encriptar lo demás, también puede probar en el relp.it que es este: https://replit.com/@JaredAmaro/Tarea9#main.py 😊

---
+++ main.py
@@ -0,0 +1,26 @@
+from cryptography.fernet import Fernet
+
+
+def genera_clave():
+  clave=Fernet.generate_key()
+  with open("clave.key","wb")as archivo_clave:
+    archivo_clave.write(clave)
+
+def cargar_clave():
+  return open("clave.key","rb").read()
+
+def encript(nom_archivo,clave):
+  f=Fernet(clave)
+  with open(nom_archivo,"rb") as file:
+    archivo_info=file.read()
+  encrypted_data=f.encrypt(archivo_info)
+  with open(nom_archivo,"wb") as file:
+    file.write(encrypted_data)
+
+def desencript(nom_archivo, clave):
+  f=Fernet(clave)
+  with open(nom_archivo,"rb") as file:
+    encrypted_data=file.read()
+  decrypted_data=f.decrypt(encrypted_data)
+  with open(nom_archivo,"wb") as file:
+    file.write(decrypted_data)
